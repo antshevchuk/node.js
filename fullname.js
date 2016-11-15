394049e55ae5336892fb395b0eed6345fa3a5be9
antshevchuk <antshevchuk@yandex.ru>
2016-11-15 20:57:04 +0500

    change Task2B

--- fullname.js
+++ fullname.js
@@ -1,9 +1,16 @@
 var http = require('http');
 var url = require('url');
+var express = require('express');
+var app = express();
+var cors = require('cors');
+var fs = require("fs");
+app.use(cors());
+
 var fio='';
 
 
-var server = new http.Server(function (req, res) {
+/*var server = new http.Server(function (req, res)*/
+app.get('/task2B/', function (req, res) {
   var urlParsed = url.parse(req.url, true);
   //console.log(urlParsed);
   if (urlParsed.query.fullname){
@@ -12,14 +19,14 @@ var server = new http.Server(function (req, res) {
 	  if (str.length>3)res.end('Invalid fullname');
 	  else if (str.length==1) {res.end(str[0]);}
 		else  if (str.length==2) {fio = str[1]+' ' +str[0].charAt(0)+'.';} 
-		  else {fio = str[2]+' ' +str[1].charAt(0)+'.'+' ' +str[0].charAt(0)+'.';}
+		  else {fio = str[2]+' ' +str[0].charAt(0)+'.'+' ' +str[1].charAt(0)+'.';}
 	  decodeURI(fio);
-	  res.end(fio);
-	  }
+	  res.send(fio);
+  }
   else {
-  res.statusCode = 404;
-  res.end('Page not found');
+  //res.statusCode = 404;
+  res.end('Invalid fullname');
   }
   
 });
-server.listen(3000, '127.0.0.1');
+app.listen(3000, '127.0.0.1');
